605822949a25fc1d8bbf5f2e5c3d3eeb478f39f4
Carlos Antonio <carlosakr@outlook.com>
2023-11-08 10:11:09 -0300

    Implementada a formatação de padrão monetário do Brasil (R$)

--- src/Ongdonate.java
+++ src/Ongdonate.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 
 public class Ongdonate {
     public static void main(String[] args) {
-        double valorDolar;
+        double valorDolar, valorReais;
         System.out.println("Informe o valor a ser doado em dólares: ");
         Scanner entrada = new Scanner(System.in);
         valorDolar = entrada.nextDouble();
-        valorDolar = valorDolar * 4.87;
-        System.out.println("Obrigado por colaborar com a nossa causa. O Valor realizado da doação após a conversão em reais é de R$ "+ valorDolar);
-
+        valorReais = valorDolar * 4.87;
+        Locale brasil = new Locale("pt", "BR");
+        NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(brasil);
+        String valorFormatado = formatoMoeda.format(valorReais);
+        System.out.println("Obrigado por colaborar com a nossa causa. O Valor realizado da doação após a conversão em reais é de "+ valorFormatado);
     }
 }
