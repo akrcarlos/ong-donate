134377786295115f85c4cc2a8b63cdc1838dcfac
Carlos Antonio <carlosakr@outlook.com>
2023-11-09 13:37:50 -0300

    Implementada a decisão de comprovante

--- src/Ongdonate.java
+++ src/Ongdonate.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-
 public class Ongdonate {
     public static void main(String[] args) {
+
+        //Entrada de valores
+
         double valorDolar, valorReais;
         System.out.println("Informe o valor a ser doado em dólares: ");
         Scanner entrada = new Scanner(System.in);
         valorDolar = entrada.nextDouble();
         valorReais = valorDolar * 4.87;
+
+        //Converter moeda
+
         Locale brasil = new Locale("pt", "BR");
         NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance(brasil);
         String valorFormatado = formatoMoeda.format(valorReais);
-        System.out.println("Obrigado por colaborar com a nossa causa. O Valor realizado da doação após a conversão em reais é de "+ valorFormatado);
+        System.out.println("Doação realizada com sucesso no valor de " + valorFormatado);
+
+        //Gerar comprovante
+
+        System.out.println("Gostaria de fazer o download do comprovante?");
+        Scanner leitor = new Scanner(System.in);
+        String resposta;
+        resposta = leitor.nextLine();
+        if(resposta.equalsIgnoreCase("sim")) {
+            System.out.println("Download realizado.");
+        } else if (resposta.equalsIgnoreCase("não") || resposta.equalsIgnoreCase("nao")) {
+            System.out.println("Obrigado!");
+        } else  System.out.println("Opção inválida. Por favor, digite 'sim' ou 'não'.");
     }
 }
